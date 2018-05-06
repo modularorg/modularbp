49d004979a479a0177b9dbb91c052c50f43fdd7d
Antoine Boulanger <hello@antoineboulanger.com>
2018-05-06 18:59:44 -0400

    Add possibility to set default modules in mconfig to skip cli questions

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const { spawn, spawnSync } = require('child_process');
 const commander = require('commander');
 const inquirer = require('inquirer');
 const log = require('./lib/log');
-const questions = require('./lib/questions');
 const cloneRepo = require('./lib/cloneRepo');
+const exists = require('./lib/exists');
 const del = require('./lib/del');
 const createFile = require('./lib/createFile');
 const installPackages = require('./lib/installPackages');
 const copy = require('./lib/copy');
 const createDir = require('./lib/createDir');
 const prefix = require('./lib/prefix');
+let questions = require('./lib/questions');
 
 const dependencies = {
     gulp: ['error', 'notify', 'paths', 'serve', 'svg', 'watch']
@@ -25,27 +26,55 @@ commander.parse(process.argv);
 function initQuestions(repo, dest = '.') {
     log(`👋 Hey, let's init your project.`);
 
-    inquirer
-        .prompt(questions)
-        .then(answers => init(answers, repo, dest));
-}
-
-function init(answers, repo, dest) {
-    log(`👌 All good, installing everything for you.`);
-
     if (repo) {
         cloneRepo(repo, dest);
     }
+
+    let destFolder = '';
+    if (dest != '.') {
+        destFolder = '/' + dest;
+    }
+
+    let config;
+    if (exists(`${dest}/mconfig.json`)) {
+        let newQuestions = [];
+        config = require(process.cwd() + destFolder + '/mconfig.json');
+
+        if (config.modules) {
+            for (let key in questions) {
+                if(!(questions[key].name in config.modules)) {
+                    newQuestions.push(questions[key]);
+                }
+            }
+
+            questions = newQuestions;
+        }
+    }
+
+    if (questions.length != 0) {
+        inquirer
+            .prompt(questions)
+            .then(answers => {
+                if (config && config.modules) {
+                    answers = Object.assign(answers, config.modules);
+                }
+                init(config, answers, repo, dest);
+            });
+    } else {
+        init(config, config.modules, repo, dest);
+    }
+}
+
+function init(config, answers, repo, dest) {
+    log(`👌 All good, installing everything for you.`);
 
     createFile(`${dest}/package.json`, '{}');
 
     installPackages('mbp', dest);
 
     const org = '@modularbp/'
-    const name = answers.name;
     const build = answers.build;
 
-    delete answers.name;
     delete answers.build;
 
     let packages = [];
@@ -63,24 +92,26 @@ function init(answers, repo, dest) {
 
     copy([`${dest}/node_modules/mbp/src/**/*`, `${dest}/node_modules/mbp/src/**/.*`], `${dest}/`);
 
-    let destFolder = '';
-    if (dest != '.') {
-        destFolder = '/' + dest;
-    }
+    if (!config) {
+        let destFolder = '';
+        if (dest != '.') {
+            destFolder = '/' + dest;
+        }
 
-    const paths = require(process.cwd() + destFolder + '/mconfig.json');
+        config = require(process.cwd() + destFolder + '/mconfig.json');
+    }
 
     installPackages(packages, dest);
 
     copy(`${dest}/node_modules/${org}${build}/src/*`, `${dest}/`);
 
-    copy(`${dest}/node_modules/${org}${build}-*/src/*`, prefix(paths.build, dest));
+    copy(`${dest}/node_modules/${org}${build}-*/src/*`, prefix(config.build, dest));
 
-    createDir(prefix(paths.styles.src, dest));
-    createDir(prefix(paths.scripts.src, dest));
-    createDir(prefix(paths.svgs.src, dest));
-    createDir(prefix(paths.views.src, dest));
-    createDir(prefix(paths.views.partials, dest));
+    createDir(prefix(config.styles.src, dest));
+    createDir(prefix(config.scripts.src, dest));
+    createDir(prefix(config.svgs.src, dest));
+    createDir(prefix(config.views.src, dest));
+    createDir(prefix(config.views.partials, dest));
 
     log(`👊 Ready to go, you can now run ${build}`);
 }
